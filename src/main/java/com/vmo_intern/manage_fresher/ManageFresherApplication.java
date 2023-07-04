1129f669ad7aacd256efa39eb4bec3aac1d3c532
tandc <tan.c.dao@vntrip.vn>
2023-07-04 17:36:49 +0700

    initial

---
+++ src/main/java/com/vmo_intern/manage_fresher/ManageFresherApplication.java
@@ -0,0 +1,13 @@
+package com.vmo_intern.manage_fresher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageFresherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageFresherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vmo_intern/manage_fresher/ManageFresherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vmo_intern.manage_fresher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageFresherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
